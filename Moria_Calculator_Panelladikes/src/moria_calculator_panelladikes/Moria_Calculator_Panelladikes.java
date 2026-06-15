15cfff4a79efb0f2246a40e274ad85fb4b6f8090
PekCode <nickmastrojohn@gmail.com>
2026-06-15 22:44:14 +0300

    Refactor Moria_Calculator_Panelladikes.java structure

--- Moria_Calculator_Panelladikes/src/moria_calculator_panelladikes/Moria_Calculator_Panelladikes.java
+++ Moria_Calculator_Panelladikes/src/moria_calculator_panelladikes/Moria_Calculator_Panelladikes.java
@@ -7,7 +7,7 @@ public class Moria_Calculator_Panelladikes {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int answer;
-
+        // Asking for the student's track
         do {
             System.out.println("\n===========================================");
             System.out.println("-- Ypologismos Moriwn Gia Tis Panelladikes --");
@@ -20,12 +20,11 @@ public class Moria_Calculator_Panelladikes {
 
             System.out.print("\nParakalw plhktrologiste ton arithmo ths kateuthinshs sas: ");
             answer = in.nextInt();
-
+            
             if (answer == 0) {
                 System.out.println("Kalo apotelesma! Termatismos programmatos...");
                 break;
             }
-
             switch (answer) {
                 case 1 -> calculateMoria(in, "Ekthesh", "Mathimatika", "Xhmeia", "Fysikh");
                 case 2 -> calculateMoria(in, "Ekthesh", "Istoria", "Arxaia", "Latinika");
@@ -40,20 +39,20 @@ public class Moria_Calculator_Panelladikes {
     }
 
     private static void calculateMoria(Scanner in, String sub1, String sub2, String sub3, String sub4) {
-        System.out.println("\nParakalw eisagete thn baruthta twn mathimatwn ths sxolhs (se %): ");
+        System.out.println("\nParakalw eisagete thn baruthta twn mathimatwn ths sxolhs (se %): ");  // Asks for the weight factors
         System.out.print("--" + sub1 + ": "); double w1 = in.nextDouble();
         System.out.print("--" + sub2 + ": "); double w2 = in.nextDouble();
         System.out.print("--" + sub3 + ": "); double w3 = in.nextDouble();
         System.out.print("--" + sub4 + ": "); double w4 = in.nextDouble();
 
-        System.out.println("\nParakalw eisagete tous bathmous sas (0-20): ");
+        System.out.println("\nParakalw eisagete tous bathmous sas (0-20): "); // Asks for the grades
         System.out.print("--" + sub1 + ": "); double g1 = in.nextDouble();
         System.out.print("--" + sub2 + ": "); double g2 = in.nextDouble();
         System.out.print("--" + sub3 + ": "); double g3 = in.nextDouble();
         System.out.print("--" + sub4 + ": "); double g4 = in.nextDouble();
 
-        double total_grade = (g1 * w1 / 100 + g2 * w2 / 100 + g3 * w3 / 100 + g4 * w4 / 100) * 1000;
+        double total_grade = (g1 * w1 / 100 + g2 * w2 / 100 + g3 * w3 / 100 + g4 * w4 / 100) * 1000; // Calculates the total grade
         
-        System.out.println("\n>>> Ta moria gia thn sxolh pou exete epileksei einai: " + total_grade);
+        System.out.println("\n>>> Ta moria gia thn sxolh pou exete epileksei einai: " + total_grade); 
     }
 }
